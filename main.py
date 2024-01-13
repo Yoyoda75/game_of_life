d2e374be5af82b7ec7edbbad44d4a1bf3d4ace89
Yoyoda75 <bertaux.clement@protonmail.com>
2024-01-13 01:01:21 +0100

    Removed render() from the main code

--- main.py
+++ main.py
@@ -1,35 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import time
 
-import numpy as np
-import scipy
-from rich import box
-from rich.align import Align
 from rich.console import Console
 from rich.live import Live
-from rich.panel import Panel
-from rich.table import Table
 
 from game.board import Board
 
-SIZE = 50
 
-
-def update_table(board: Board) -> Table:
-    table = Table(title="Game of Life", show_header=False, show_footer=False, show_edge=False, box=None)
-    table.padding = (0, 0)
-    table.pad_edge = False
-
-    columns = board.shape[1]
-    for column in range(columns):
-        table.add_column(no_wrap=True, justify="center", vertical="middle")
-
-    for row in board:
-        formatted_row = ["■" if element else " " for element in row]
-        table.add_row(*formatted_row)
-
-    return table
+SIZE = 30
 
 
 def main():
@@ -37,15 +16,12 @@ def main():
     board = Board(SIZE)
     board.init_random_board()
 
-    # print(board)
-    # print(board.neighbors)
-    repeats = 100000
-    refresh = 50
+    repeats = 1_000_000
+    refresh = 60
     with Live(refresh_per_second=refresh, screen=True) as live:
-        for _ in range(repeats):
+        for step in range(repeats):
             time.sleep(1 / refresh)
-            table = Align.center(update_table(board), vertical="middle")
-            live.update(table)
+            live.update(board.render(step))
             board.next_step()
 
 
